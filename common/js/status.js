25d28486308503103dc19f328da47e86616a101e
KAITOKID3rd <101561058+KAITOKID3rd@users.noreply.github.com>
2023-01-19 19:15:53 +0900

    Update status.js

--- common/js/status.js
+++ common/js/status.js
@@ -10,6 +10,6 @@ var status_relay_server= 0;
 {
 if (status_server1 == 0) {
   // id名pass_failのHTML内に「合格です」というテキストを入力する
-  document.getElementById("server1").innerHTML ="<div id="server1" class="spinner-grow text-success" role="status"><span class="sr-only"></span></div>";
+  document.getElementById("server1").innerHTML ="<div class="spinner-grow text-success" role="status"><span class="sr-only"></span></div>";
 } else {
-  document.getElementById("server1").innerHTML ="<div id="server1" class="spinner-grow text-danger" role="status"><span class="sr-only"></span></div>";}
+  document.getElementById("server1").innerHTML ="<div class="spinner-grow text-danger" role="status"><span class="sr-only"></span></div>";}
